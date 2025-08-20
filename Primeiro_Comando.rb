6747ff98a4bd3bd46b19ab7d5d4987d01e96d40b
Ana R. Carvalho <137975286+AnaRaquelCarvalho@users.noreply.github.com>
2025-08-20 16:33:07 -0300

    Create Primeiro_Comando.rb

---
+++ Primeiro_Comando.rb
@@ -0,0 +1,21 @@
+print('--------------------------------')
+print" Primeiros Comandos do Ruby\n "
+print('--------------------------------')
+
+# Comandos de saída de dados
+print 'Hello World"!' # Precisa de \n para pular linha
+print 'Olá, Mundo!'
+
+puts 'Hello world!'   # Quebra linha automática
+puts 'Olá Mundo!'
+
+# Comentários:
+# <- Este é um comentário de uma linha
+
+# Comentário Multi - linha (usando =begin e =end)
+
+=begin
+    Estou aprendendo a linguagem Ruby na 
+    One Bit Code e estou gostando muito!
+=end    
+
