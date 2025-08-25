aa96f42a724b2e800c0f3f4e1939bc50d952e2eb
Ana R. Carvalho <137975286+AnaRaquelCarvalho@users.noreply.github.com>
2025-08-25 16:54:32 -0300

    Update Calculo_IMC.rb

--- Calculo_IMC.rb
+++ Calculo_IMC.rb
@@ -1,26 +1,48 @@
-puts'----------------'
-puts'CALCULO DO IMC'
-puts'---------------'
+puts '----------------'
+puts 'CALCULO DO IMC'
+puts '---------------'
 
 puts "Categorias IMC"
-puts "< 16,5 - Peso muito abaixo do normal"
-puts "< 18,5 - Peso abaixo do normal"
-puts "18,5 - 24,99 = Peso normal"
-puts "25 - 29,99 = Pré-obeso"
-puts "30 - 34,99 = Obesidade Classe I"
-puts "35 - 39,99 = Obesidade Classe II"
-puts "> 40 = Obesidade Classe III"
+puts "< 16,5  - Peso muito abaixo do normal"
+puts "16,5 - 18,4  - Peso abaixo do normal"
+puts "18,5 - 24,99 - Peso normal"
+puts "25 - 29,99   - Pré-obeso"
+puts "30 - 34,99   - Obesidade Classe I"
+puts "35 - 39,99   - Obesidade Classe II"
+puts ">= 40        - Obesidade Classe III"
+puts'-' * 40
 
 # Calculadora IMC
 puts "Calculadora IMC"
 
-puts "Informe a altura (cm):"
+print "Informe a altura (cm): "
 height = gets.chomp.to_f
 
-puts "Informe o peso (kg):"
+print "Informe o peso (kg): "
 weight = gets.chomp.to_f
 
-bmi = weight / (height/100) **2
-puts "IMC = #{bmi}"
+# cálculo do IMC
+bmi = weight / (height / 100) ** 2
 
-puts'---------------------------------------'
+# sempre com duas casas decimais
+puts "Seu IMC é: #{'%.2f' % bmi}"
+
+# Estrutura case com intervalos
+case bmi
+when 0...16.5
+  puts "Peso muito abaixo do normal"
+when 16.5...18.5
+  puts "Peso abaixo do normal"
+when 18.5...25
+  puts "Peso normal"
+when 25...30
+  puts "Pré-obeso"
+when 30...35
+  puts "Obesidade Classe I"
+when 35...40
+  puts "Obesidade Classe II"
+else
+  puts "Obesidade Classe III"
+end
+
+puts'-' * 40
